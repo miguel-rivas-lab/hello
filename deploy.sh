752cbd1507f76198c6ca0cb2c1955bd255f5d6d6
Miguel <jemiguelrivas@gmail.com>
2021-03-24 03:21:53 -0400

    fixing favicon

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:miguel-rivas/nano-grid.git master:gh-pages
+git push -f git@github.com:miguel-rivas/hello.git master:gh-pages
 
 cd -
